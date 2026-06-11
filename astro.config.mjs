ce08693680fc1fd24accf8748deb74aef5378f30
wxjbnu <wxjbnu2023@gmail.com>
2026-06-11 11:24:06 +0800

    Update site URL and title for empty-pixel.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://empty-pixel.com',
 	output: 'static',
 	integrations: [mdx(), sitemap()],
 	fonts: [
